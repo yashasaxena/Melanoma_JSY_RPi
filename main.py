8fa9058aff551278f3733da99e1af9e2624599ac
[yashasaxena] <[yasha.saxena@gmail.com]>
2017-12-06 20:36:38 -0500

    added proper URL

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import requests
 import base64
+import watchdog
 
 
 #file = filepath of image on pi
 #image = open(file, 'rb')
 #image_read = image.read()
 #image_64_encode = base64.encodestring(image_read)
+#assumes 1 image is sent at a time
 
 image_data = {'image_b64': image_64_encode}
 
