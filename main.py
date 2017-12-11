9ec869db7bcc448148c479a788fa6cf2c67bcd8e
[yashasaxena] <[yasha.saxena@gmail.com]>
2017-12-11 15:11:43 -0500

    modifications to RPi client file to include multiple images, proper dictionary loading, csv file writing

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import requests
+import json
 import base64
 import glob
 import os
+import csv
 #import watchdog
 
 
@@ -23,18 +25,35 @@ files = glob.glob('/media/usbstick/Melanoma Images/'+'*.jpg')
 
 #glob to extract all image files
 
+c = 0
+
 for img in files:
     with open(img, 'rb') as image_read:
         image_64_encode = base64.b64encode(image_read.read())
-        image_data["test_image"].append(image_64_encode)
+        image_64_encode_bytes = image_64_encode.decode('utf-8')
+        image_key = "image" + str(c)
+        image_data[image_key].append([image_64_encode_bytes])
+        c = c + 1
 
-
-
+# store post request data in variable r
 r = requests.post(url, json=image_data)
 
+result_data = r.json()
 
+# writing results from json results to a csv file
 
+headers = "% Malignant, % Benign/n"
+results = result_data[1]['prediction']
+c = 0
 
+results_file = open('diagnosis.csv', 'w')
+results_file.write(headers)
+
+for x in range(0, len(results_data)-1):
+    key = 'prediction' + str(c)
+    row = results_data[x+1][key][0] + ',' results_data[x+1][key][1]
+    csv.write(row)
+    c = c+1
 
 # r.text
 # {
